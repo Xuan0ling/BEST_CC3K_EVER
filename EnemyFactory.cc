10521158cc85557fed006a290c809025ebdf6cfe
Vaishnav Mandru <106818193+Vaishnav469@users.noreply.github.com>
2024-07-24 05:12:13 -0400

    enemy chanhe

--- EnemyFactory.cc
+++ EnemyFactory.cc
@@ -1,12 +1,12 @@
 #include "enemyFactory.h"
 #include "enemy.h"
 
-Enemy EnemyFactory::createEnemy(Floor *floor, Posn posn) {
-    return Enemy(floor,' ',posn, 0, 0, 0);
+Enemy* EnemyFactory::createEnemy(Floor *floor, Posn posn) {
+    return new Enemy(floor,' ',posn, 0, 0, 0);
 }
 
-Enemy EnemyFactory::createEnemy(Floor *floor, Posn posn, Item* item) {
-    return Enemy(floor, ' ',posn, 0, 0, 0);
+Enemy* EnemyFactory::createEnemy(Floor *floor, Posn posn, Item* item) {
+    return new Enemy(floor, ' ',posn, 0, 0, 0);
 }
 
 EnemyFactory::~EnemyFactory() {}
